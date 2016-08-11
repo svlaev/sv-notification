01fbac03e5623d2eb54422e2e4b122daa14b7cca
Stanislav Vlaev <stanislav.vlaev@gmail.com>
2016-08-11 15:06:11 +0300

    WiP: Recreating the whole pod

---
+++ SVNotification/SVNotification/SVNotification.h
@@ -0,0 +1,17 @@
+//
+//  SVNotification.h
+//  SVNotification
+//
+//  Created by Stanislav Vlaev on 8/11/16.
+//  Copyright © 2016 sv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNotification.
+FOUNDATION_EXPORT double SVNotificationVersionNumber;
+
+//! Project version string for SVNotification.
+FOUNDATION_EXPORT const unsigned char SVNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNotification/PublicHeader.h>
